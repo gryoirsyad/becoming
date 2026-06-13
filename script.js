fa552908cca2105d688cbec74bfac08c062fc668
gryoirsyad <gryoirsyad@apps.ipb.ac.id>
2026-06-13 20:40:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,35 @@
-function updateClock(){
-
+function updateClock() {
     const now = new Date();
 
-    const time =
-        now.toLocaleTimeString("id-ID");
+    const time = now.toLocaleTimeString("id-ID");
 
-    const date =
-        now.toLocaleDateString(
-            "id-ID",
-            {
-                weekday:"long",
-                year:"numeric",
-                month:"long",
-                day:"numeric"
-            }
-        );
+    const date = now.toLocaleDateString(
+        "id-ID",
+        {
+            weekday: "long",
+            year: "numeric",
+            month: "long",
+            day: "numeric"
+        }
+    );
 
-    document.getElementById(
-        "clock"
-    ).innerText = time;
-
-    document.getElementById(
-        "date"
-    ).innerText = date;
+    document.getElementById("clock").textContent = time;
+    document.getElementById("date").textContent = date;
 
     const hour = now.getHours();
 
     let greeting = "";
 
-    if(hour < 12){
+    if (hour < 12) {
         greeting = "Selamat pagi ☀";
-    }
-    else if(hour < 18){
+    } else if (hour < 18) {
         greeting = "Selamat sore 🌤";
-    }
-    else{
+    } else {
         greeting = "Selamat malam 🌙";
     }
 
-    document.getElementById(
-        "greeting"
-    ).innerText = greeting;
+    document.getElementById("greeting").textContent = greeting;
 }
 
 updateClock();
-
-setInterval(updateClock,1000);
+setInterval(updateClock, 1000);
