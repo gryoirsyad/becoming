183d0218da8bc96cdd73c548fdf23f04270b0a52
gryoirsyad <gryoirsyad@apps.ipb.ac.id>
2026-06-13 19:18:53 +0700

    Add clock update and greeting functionality

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("SenaOS loaded");
+function updateClock(){
+
+    const now = new Date();
+
+    const time = now.toLocaleTimeString();
+
+    document.getElementById("time")
+    .innerHTML = time;
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
+
+
+
+const hour = new Date().getHours();
+
+let greeting = "";
+
+if(hour < 12){
+    greeting = "Good Morning, Sena";
+}
+else if(hour < 18){
+    greeting = "Good Afternoon, Sena";
+}
+else{
+    greeting = "Good Evening, Sena";
+}
+
+document.getElementById("greeting")
+.innerHTML = greeting;
